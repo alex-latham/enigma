0150eab5537af9a7ec76fdd148c4695ed1bab481
Alex Latham <alexlthm@gmail.com>
2020-02-29 09:11:02 -0700

    Add dictionary generator and attribute with reader

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,18 @@
 require 'date'
 
 class Enigma
+  attr_reader :dictionary
+
+  def initialize
+    @dictionary = generate_dictionary
+  end
+
+  def generate_dictionary
+    keys = (1..27).to_a
+    values = ("a".."z").to_a << " "
+    keys.zip(values).to_h
+  end
+
   def generate_key
     unpadded_key = rand(99999).to_s
     "0" * (5 - unpadded_key.length) + unpadded_key
@@ -11,9 +23,4 @@ class Enigma
     date_squared.to_s[-4..-1]
   end
 
-  def generate_dictionary
-    keys = (1..27).to_a
-    values = ("a".."z").to_a << " "
-    keys.zip(values).to_h
-  end
 end
