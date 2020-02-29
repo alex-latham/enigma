9be51377c76ddab1051f873caa49f9b8326ac7d8
Alex Latham <alexlthm@gmail.com>
2020-02-28 17:43:58 -0700

    Add test for generating a key

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,4 +9,12 @@ class EnigmaTest < Minitest::Test
   def test_it_can_exist
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_can_generate_a_key
+    assert_instance_of String, @enigma.generate_key
+    assert_equal 5, @enigma.generate_key.length
+    @enigma.generate_key.each_char do |character|
+      assert_instance_of Integer, character
+    end
+  end
 end
