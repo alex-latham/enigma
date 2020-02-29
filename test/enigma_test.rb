9f6f456f645848ac335dbd114e1078ffe913a023
Alex Latham <alexlthm@gmail.com>
2020-02-28 21:10:05 -0700

    Final generate_offset test format including edge case (all zero), fix assertion for regex match in generate_key test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,4 +1,5 @@
 require_relative 'test_helper'
+require 'date'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
@@ -15,12 +16,16 @@ class EnigmaTest < Minitest::Test
 
     assert_instance_of String, key
     assert_equal 5, key.length
-    assert key.match(/^(\d)+$/)
+    assert_equal true, key.match?(/^(\d)+$/)
   end
 
   def test_it_can_generate_an_offset
-    offset = @enigma.generate_offset("040895")
+    offset1 = @enigma.generate_offset("040895")
 
-    assert_equal 1025, offset
+    Date.expects(:today).returns(Date.new(2000, 01, 01))
+    offset2 = @enigma.generate_offset
+
+    assert_equal "1025", offset1
+    assert_equal "0000", offset2
   end
 end
