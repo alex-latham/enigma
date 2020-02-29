fbb617427729ce4983529d861c8677b56265e90a
Alex Latham <alexlthm@gmail.com>
2020-02-29 09:09:46 -0700

    Add dictionary testing

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,10 +4,24 @@ require './lib/enigma'
 class EnigmaTest < Minitest::Test
   def setup
     @enigma = Enigma.new
+    @dictionary = {
+      1=>"a", 2=>"b", 3=>"c", 4=>"d", 5=>"e", 6=>"f", 7=>"g", 8=>"h",
+      9=>"i", 10=>"j", 11=>"k", 12=>"l", 13=>"m", 14=>"n", 15=>"o",
+      16=>"p", 17=>"q", 18=>"r", 19=>"s", 20=>"t", 21=>"u", 22=>"v",
+      23=>"w", 24=>"x", 25=>"y", 26=>"z", 27=>" "
+    }
   end
 
   def test_it_can_exist
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_has_attributes
+    assert_equal @dictionary, @enigma.dictionary
+  end
+
+  def test_it_can_generate_a_dictionary
+    assert_equal @dictionary, @enigma.generate_dictionary
   end
 
   def test_it_can_generate_a_random_key
@@ -27,14 +41,6 @@ class EnigmaTest < Minitest::Test
     assert_equal "0000", offset2
   end
 
-  def test_it_can_generate_a_dictionary
-    expected = { 1=>"a", 2=>"b", 3=>"c", 4=>"d", 5=>"e", 6=>"f", 7=>"g", 8=>"h",
-                 9=>"i", 10=>"j", 11=>"k", 12=>"l", 13=>"m", 14=>"n", 15=>"o",
-                 16=>"p", 17=>"q", 18=>"r", 19=>"s", 20=>"t", 21=>"u", 22=>"v",
-                 23=>"w", 24=>"x", 25=>"y", 26=>"z", 27=>" " }
-
-    assert_equal expected, @enigma.generate_dictionary
-  end
 
   def test_it_can_encrypt_a_message
     skip
