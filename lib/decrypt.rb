07727c7d46d88e9e81a40ab7f955dc13ce9b653d
Alex Latham <alexlthm@gmail.com>
2020-03-01 20:03:16 -0700

    Reorganize encrypt.rb and decrypt.rb into related sections

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,11 +2,11 @@ require './lib/enigma'
 
 encrypted = File.open(ARGV[0], "r")
 ciphertext = encrypted.read
-decrypted = File.open(ARGV[1], "w")
 
 enigma = Enigma.new
-
 decryption_data = enigma.decrypt(ciphertext, ARGV[2], ARGV[3])
+
+decrypted = File.open(ARGV[1], "w")
 decrypted.write(decryption_data[:decryption])
 decrypted.close
 
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -2,11 +2,11 @@ require './lib/enigma'
 
 message = File.open(ARGV[0], "r")
 plaintext = message.read
-encrypted = File.open(ARGV[1], "w")
 
 enigma = Enigma.new
-
 encryption_data = enigma.encrypt(plaintext, ARGV[2], ARGV[3])
+
+encrypted = File.open(ARGV[1], "w")
 encrypted.write(encryption_data[:encryption])
 encrypted.close
 
