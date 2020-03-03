30d71e1a049848ea50d88ab9db63533afaa54e7c
Alex Latham <alexlthm@gmail.com>
2020-03-02 21:35:35 -0700

    Add linebreaks to CLI files for readability.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -10,4 +10,5 @@ encrypted = File.open(ARGV[1], "w")
 encrypted.write(encryption_data[:encryption])
 encrypted.close
 
-p "Created '#{ARGV[1]}' with the key #{encryption_data[:key]} and date #{encryption_data[:date]}"
+p "Created '#{ARGV[1]}' with the key #{encryption_data[:key]}\
+ and date #{encryption_data[:date]}"
