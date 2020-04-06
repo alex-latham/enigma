a4b5add1a7c2b88ba85f1357a5b3a7ccb0c9d4e1
Alex Latham <alexlthm@gmail.com>
2020-04-06 10:13:40 -0600

    Standardize hashes to follow symbol => integer pattern

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,21 +2,23 @@ require './lib/cipher'
 
 class Enigma < Cipher
   def generate_key
-    rand(99999).to_s.rjust(5, "0")
-  end
-
-  def generate_shifts(key, date, direction)
-    keys = {a: key[0..1], b: key[1..2], c: key[2..3], d: key[3..4]}
-    offsets = generate_offsets(date)
-    keys.merge(offsets) do |_, key_shift, offset_shift|
-      (key_shift.to_i + offset_shift.to_i) * direction
-    end
+    rand(99999).to_s.rjust(5, '0')
   end
 
   def generate_offsets(date)
     date_squared = date.to_i**2
     offset = date_squared.to_s[-4..-1]
-    {a: offset[0], b: offset[1], c: offset[2], d: offset[3]}
+    offset_shifts = {a: offset[0], b: offset[1], c: offset[2], d: offset[3]}
+    offset_shifts.transform_values(&:to_i)
+  end
+
+  def generate_shifts(key, date, direction)
+    keys = {a: key[0..1], b: key[1..2], c: key[2..3], d: key[3..4]}
+    keys.transform_values!(&:to_i)
+    offsets = generate_offsets(date)
+    keys.merge(offsets) do |_, key, offset|
+      (key + offset) * direction
+    end
   end
 
   def mutate_string(string, shifts)
