a0d7689853201e18b1e1aefaacdfbc23c47ddfb7
Alex Latham <alexlthm@gmail.com>
2020-04-06 09:29:29 -0600

    Move generable module methods into enigma superclass

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,39 @@
-require 'date'
-require './lib/generable'
+require './lib/cipher'
 
-class Enigma
-  include Generable
-  attr_reader :charset
+class Enigma < Cipher
+  def generate_key
+    rand(99999).to_s.rjust(5, "0")
+  end
 
-  def initialize
-    @charset = ("a".."z").to_a << " "
+  def generate_shifts(key, date, direction)
+    keys = {a: key[0..1], b: key[1..2], c: key[2..3], d: key[3..4]}
+    offsets = generate_offsets(date)
+    keys.merge(offsets) do |_, key_shift, offset_shift|
+      (key_shift.to_i + offset_shift.to_i) * direction
+    end
+  end
+
+  def generate_offsets(date)
+    date_squared = date.to_i**2
+    offset = date_squared.to_s[-4..-1]
+    {a: offset[0], b: offset[1], c: offset[2], d: offset[3]}
+  end
+
+  def mutate_string(string, shifts)
+    mutated_string = String.new
+    string.each_char.with_index do |char, index|
+      if @charset.include?(char)
+        mutated_string += shift_charset(index, shifts)[@charset.index(char)]
+      end
+      mutated_string += char unless @charset.include?(char)
+    end
+    mutated_string
+  end
+
+  def shift_charset(index, shifts)
+    return @charset.rotate(shifts[:a]) if index % 4 == 0
+    return @charset.rotate(shifts[:b]) if index % 4 == 1
+    return @charset.rotate(shifts[:c]) if index % 4 == 2
+    return @charset.rotate(shifts[:d]) if index % 4 == 3
   end
 end
