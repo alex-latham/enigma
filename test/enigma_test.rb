754074b8c9f0ae32548685db54c20f918ca4cd43
Alex Latham <alexlthm@gmail.com>
2020-04-06 10:53:28 -0600

    Reduce lines for expected shifted charset

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,15 +10,15 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_it_can_generate_a_key
+    @enigma.stubs(:rand).returns(923)
+    assert_equal '00923', @enigma.generate_key
+  end
+
   def test_it_can_generate_offsets
     expected = {a: 1, b: 0, c: 2, d: 5}
 
     assert_equal expected, @enigma.generate_offsets('040895')
-  end
-
-  def test_it_can_generate_a_key
-    @enigma.stubs(:rand).returns(923)
-    assert_equal '00923', @enigma.generate_key
   end
 
   def test_it_can_generate_shifts
@@ -34,8 +34,7 @@ class EnigmaTest < Minitest::Test
   def test_it_can_shift_the_charset
     shifts = {a: 3, b: 27, c: 73, d: 20}
     expected = ['t', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 'a', 'b', 'c', 'd',
-                'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-                'q', 'r', 's']
+      'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's']
 
     assert_equal expected, @enigma.shift_charset(14, shifts)
   end
